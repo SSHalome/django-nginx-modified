b2d6115aa886d00d9993aa6b6bd5ee41e44360d0
root <root@ip-172-31-46-34.eu-central-1.compute.internal>
2023-06-14 08:07:39 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # django-nginx-modified
+# django-nginx-ECS
--- skript.sh
+++ skript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "# django-nginx-modified" >> README.md
+echo "# django-nginx-ECS" >> README.md
 git init
 git add README.md
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://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@github.com/SSHalome/django-nginx-modified.git
+git remote add origin git@github.com:SSHalome/django-nginx-ECS.git
 git push -u origin main
